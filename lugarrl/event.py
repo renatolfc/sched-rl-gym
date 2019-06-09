e69a714f8a6715d0a24427fd26223ff92189c1a5
Renato L. de F. Cunha <renatocunha@acm.org>
2019-06-09 10:33:47 -0300

    event: add job events and start/finish event types

--- lugarrl/event.py
+++ lugarrl/event.py
@@ -1,32 +1,74 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import copy
 import enum
 import warnings
+from typing import List, Optional, Iterable, TypeVar, Generic, Iterator
 
+from job import Job
 from .heap import Heap
-from .resource_pool import ResourceType, IntervalTree
+from .resource_pool import ResourceType, Interval
+
+T = TypeVar('T', bound='Event')
+
 
 class EventType(enum.IntEnum):
-    ALLOCATION = 0
-    DEALLOCATION = 1
+    RESOURCE_ALLOCATE = 0
+    RESOURCE_FREE = 1
+    JOB_START = 3
+    JOB_FINISH = 4
 
 
-class ResourceEvent:
-    def __init__(self, time, event_type: EventType, resource_type: ResourceType, resources: IntervalTree):
+class Event(object):
+    time: int
+    event_type: EventType
+
+    def __init__(self, time: int, event_type: EventType):
         self.time = time
-        self.resources = resources
         self.event_type = event_type
+
+    def clone(self):
+        return copy.copy(self)
+
+
+class ResourceEvent(Event):
+    resources: Iterable[Interval]
+    resource_type: ResourceType
+
+    def __init__(self, time: int, event_type: EventType, resource_type: ResourceType, resources: Iterable[Interval]):
+        super().__init__(time, event_type)
+        self.resources = resources
         self.resource_type = resource_type
 
 
-class ResourceEventQueue:
-    def __init__(self, time=0):
+class JobEvent(Event):
+    job: Job
+
+    def __init__(self, time: int, event_type: EventType, job: Job):
+        super().__init__(time, event_type)
+        self.job = job
+
+    @property
+    def processors(self):
+        return self.job.requested_processors
+
+    @property
+    def memory(self):
+        return self.job.requested_memory
+
+
+class EventQueue(Generic[T]):
+    time: int
+    future: Heap
+    past: List[T]
+
+    def __init__(self, time: int = 0):
         self.past = []
         self.time = time
         self.future = Heap()
 
-    def add(self, event: ResourceEvent):
+    def add(self, event: T):
         if event.time >= self.time:
             self.future.add(event, event.time)
         else:
@@ -35,11 +77,11 @@ class ResourceEventQueue:
             warnings.warn('Adding events to the past might change the ordering of '
                           'events that happened at the same time.')
 
-    def step(self, time=1):
+    def step(self, time: int = 1) -> Iterable[T]:
         if time < 0:
             raise AssertionError("Tried to move into the past.")
         self.time += time
-        present = []
+        present: List[T] = []
         first = self.future.first
         while first and first.time <= self.time:
             current = self.future.pop()
@@ -49,11 +91,14 @@ class ResourceEventQueue:
         return present
 
     @property
-    def next(self):
+    def next(self) -> Optional[T]:
         if len(self.future) == 0:
             return None
         return self.future.first
 
     @property
-    def last(self):
-        return self.past[-1]
+    def last(self) -> Optional[T]:
+        return self.past[-1] if self.past else None
+
+    def __iter__(self) -> Iterator[T]:
+        return self.future.heapsort()
