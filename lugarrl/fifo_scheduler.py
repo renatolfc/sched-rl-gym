ff55fbd0c8f18342304b0807278903c1fd055646
Renato L. de F. Cunha <renatocunha@acm.org>
2019-06-11 09:24:02 -0300

    fifo_scheduler: fix fifo scheduling

--- lugarrl/fifo_scheduler.py
+++ lugarrl/fifo_scheduler.py
@@ -1,36 +1,40 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from typing import Tuple, Iterable, Optional
+from typing import Tuple, Iterable
+from collections import defaultdict
 
 from .job import Job, JobStatus
 from .scheduler import Scheduler
-from .resource_pool import ResourcePool
+from .resource_pool import Interval
 
 
 class FifoScheduler(Scheduler):
     def schedule(self):
         for job in self.queue_admission:
-            time, resources = self.find_first_time_for(job)
-            processors = resources.find(job.requested_processors)
+            time, processors = self.find_first_time_for(job)
             self.add_job_events(job, processors, time)
             job.status = JobStatus.WAITING
             job.processor_list = processors
+            job.start_time = time
             self.queue_waiting.append(job)
         self.queue_admission.clear()
 
-    def find_first_time_for(self, job: Job) -> Tuple[int, ResourcePool]:
-        resources, pool = self.fits(self.current_time, job, self.processor_pool.clone(), self.job_events)
-        if resources:
-            return self.current_time, pool
-
-        for event in self.job_events:
-            near_future = [e for e in self.job_events if e.time <= event.time]
-            pool = self.play_events(
-                near_future,
-                self.processor_pool.clone()
-            )
-            resources = self.fits(event.time, job, pool, self.job_events)
+    def find_first_time_for(self, job: Job) -> Tuple[int, Iterable[Interval]]:
+        if (not self.job_events.next) or self.job_events.next.time > self.current_time:
+            resources = self.fits(self.current_time, job, self.processor_pool, self.job_events)
             if resources:
-                return event.time, pool
+                return self.current_time, resources
+
+        near_future = defaultdict(list)
+        for e in self.job_events:
+            near_future[e.time].append(e)
+
+        resource_pool = self.processor_pool.clone()
+        for time in sorted(near_future):
+            resource_pool = self.play_events(near_future[time], resource_pool)
+            resources = self.fits(time, job, resource_pool, self.job_events)
+            if resources:
+                return time, resources
+
         raise AssertionError('Failed to find time for job, even in the far future.')
