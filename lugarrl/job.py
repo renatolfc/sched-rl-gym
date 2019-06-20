6ef3eb1fb12e1e3dc1072157964d404bd91ed863
Renato L. F. Cunha <renatocunha@acm.org>
2019-06-20 08:59:54 -0300

    resource: make a shallow copy of input resources

--- lugarrl/job.py
+++ lugarrl/job.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import copy
 import enum
 
 import random
@@ -40,8 +41,8 @@ class Resource(object):
     def __init__(self, processors: IntervalTree = IntervalTree(), memory: IntervalTree = IntervalTree(),
                  ignore_memory: bool = False):
         self.ignore_memory = ignore_memory
-        self.processors = IntervalTree([i for i in processors])
-        self.memory = IntervalTree([i for i in memory])
+        self.processors = copy.copy(processors)
+        self.memory = copy.copy(memory)
 
     def measure(self) -> Tuple[int, int]:
         processors = sum([i.end - i.begin for i in self.processors])
