e13cce9ea340a39968920b084a652deec185605b
Renato L. F. Cunha <renatocunha@acm.org>
2019-06-22 20:56:25 -0300

    code: add initial random scheduler implementation

---
+++ lugarrl/random_scheduler.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import random
+
+from .scheduler import Scheduler
+
+
+class RandomScheduler(Scheduler):
+    def schedule(self) -> None:
+        while any([self.can_schedule_now(j) for j in self.queue_admission]):
+            job = self.queue_admission[random.randint(0, len(self.queue_admission))]
+            resources = self.can_schedule_now(job)
+            if resources:
+                self.assign_schedule(job, resources, self.current_time)
+                self.queue_admission.remove(job)
