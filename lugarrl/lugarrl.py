d6a66838e89f45f1d681a930324575a74956099d
Renato L. F. Cunha <renatocunha@acm.org>
2019-06-23 08:55:20 -0300

    code: optimize imports

--- lugarrl/lugarrl.py
+++ lugarrl/lugarrl.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from lugarrl.scheduler import fifo_scheduler
+from . import simulator, workload, job
 from .job import JobParameters
 from .scheduler import Scheduler
 from .simulator import Simulator
 from .workload import WorkloadGenerator
-from . import simulator, workload, job
-from lugarrl.scheduler import fifo_scheduler
 
 
 class LugarRL(object):
