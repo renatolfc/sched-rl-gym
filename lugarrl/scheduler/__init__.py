293a225d77b2e136ba1dcdca5e2dec648541b972
Renato L. F. Cunha <renatocunha@acm.org>
2019-10-15 21:54:12 -0300

    scheduler: add null scheduler

--- lugarrl/scheduler/__init__.py
+++ lugarrl/scheduler/__init__.py
@@ -4,15 +4,7 @@
 from .scheduler import Scheduler
 from .sjf_scheduler import SjfScheduler
 from .fifo_scheduler import FifoScheduler
+from .null_scheduler import NullScheduler
 from .packer_scheduler import PackerScheduler
 from .random_scheduler import RandomScheduler
 from .tetris_scheduler import TetrisScheduler
-
-__all__ = [
-    Scheduler,
-    SjfScheduler,
-    FifoScheduler,
-    PackerScheduler,
-    RandomScheduler,
-    TetrisScheduler,
-]
