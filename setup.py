20513dc0a6f656e22e83389818e46ba72e44317e
Renato L. F. Cunha <renatocunha@acm.org>
2020-10-11 20:14:14 -0300

    setup: add setup script for installation with pip

---
+++ setup.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""A setuptools based setup module.
+See:
+https://packaging.python.org/guides/distributing-packages-using-setuptools/
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+import pathlib
+import lugarrl
+
+here = pathlib.Path(__file__).parent.resolve()
+
+# Get the long description from the README file
+long_description = (here / 'README.rst').read_text(encoding='utf-8')
+
+extras = {
+    'render': [
+        'matplotlib',
+        'pygame',
+    ],
+    'test': [
+        'pytest',
+        'coverage',
+    ],
+}
+
+extras['all'] = [item for group in extras.values() for item in group]
+
+# Arguments marked as "Required" below must be included for upload to PyPI.
+# Fields marked as "Optional" may be commented out.
+
+setup(
+    name='sched-rl-gym',
+    description='OpenAI Gym environment for HPC job scheduling',
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+    url='https://github.com/renatolfc/sched-rl-gym',
+    author='Renato L. de F. Cunha',
+    author_email='renatocunha@acm.org',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+
+        # Pick your license as you wish
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate you support Python 3. These classifiers are *not*
+        # checked by 'pip install'. See instead 'python_requires' below.
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+
+    keywords='gym, reinforcement learning, artificial intelligence',
+
+    package_dir={'': 'lugarrl'},
+    packages=find_packages(where='lugarrl'),
+    python_requires='>=3.6, <4',
+
+    # This field lists other packages that your project depends on to run.
+    # Any package you put here will be installed by pip when your project is
+    # installed, so they must be valid existing projects.
+    #
+    # For an analysis of "install_requires" vs pip's requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=[
+        'gym',
+        'numpy',
+        'intervaltree>=3.0'
+    ],
+
+    extras_require=extras,
+
+    # If there are data files included in your packages that need to be
+    # installed, specify them here.
+    package_data={  # Optional
+        'sample': ['package_data.dat'],
+    },
+
+    # Although 'package_data' is the preferred approach, in some case you may
+    # need to place data files outside of your packages. See:
+    # http://docs.python.org/distutils/setupscript.html#installing-additional-files
+    #
+    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
+    data_files=[('my_data', ['data/data_file'])],  # Optional
+
+    # List additional URLs that are relevant to your project as a dict.
+    #
+    # This field corresponds to the "Project-URL" metadata fields:
+    # https://packaging.python.org/specifications/core-metadata/#project-url-multiple-use
+    #
+    # Examples listed include a pattern for specifying where the package tracks
+    # issues, where the source is hosted, where to say thanks to the package
+    # maintainers, and where to support the project financially. The key is
+    # what's used to render the link text on PyPI.
+    project_urls={  # Optional
+        'Bug Reports': 'https://github.com/renatolfc/sched-rl-gym/issues',
+        'Say Thanks!': 'https://saythanks.io/to/renatocunha%40acm.org',
+        'Source': 'https://github.com/renatolfc/sched-rl-gym',
+    },
+)
