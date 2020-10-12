bb214fd3eb38a736653d8201cf946318ea74ddbb
Renato L. F. Cunha <renatocunha@acm.org>
2020-10-12 14:40:45 -0300

    docs: document the job module

--- lugarrl/job.py
+++ lugarrl/job.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""job - Classes for jobs in the simulator.
+"""
+
 import enum
 
 import random
@@ -10,6 +13,7 @@ from .resource import Resource, PrimaryResource
 
 
 class JobStatus(enum.IntEnum):
+    """An enumeration for different states of a job within our simulator."""
     SUBMITTED = 0
     RUNNING = 1
     WAITING = 2
@@ -18,6 +22,10 @@ class JobStatus(enum.IntEnum):
 
 
 class SwfJobStatus(enum.IntEnum):
+    """An enumeration for different states of a job in the SWF_.
+
+    .. _SWF: https://www.cs.huji.ac.il/labs/parallel/workload/swf.html
+    """
     FAILED = 0
     COMPLETED = 1
     PARTIAL_TO_BE_CONTINUED = 2
@@ -27,12 +35,32 @@ class SwfJobStatus(enum.IntEnum):
     MEANINGLESS = -1
 
 
-class Job(object):
+class Job:
+    """A job in the system.
+
+    This follows the fields of the `Standard Workload Format
+    <https://www.cs.huji.ac.il/labs/parallel/workload/swf.html>`_ with a couple
+    of helper methods to compute slowdown and bounded slowdown of a job. The
+    initializer arguments follow the same ordering and have the same meaning
+    than those in the SWF description.
+
+    This makes use of the :class:`lugarrl.resource.Resource` class to keep
+    track of the assigned resources to the job. Resource assignment itself is
+    performed by :func:`lugarrl.scheduler.scheduler.Scheduler.assign_schedule`.
+
+    The figure below shows the relationship between jobs, resources, and the
+    basic data structure for resource management (`IntervalTree`).
+
+    .. image:: /img/job-resource.svg
+    """
     resources: Resource
 
-    def __init__(self, job_id, submission_time, execution_time, processors_allocated, average_cpu_use, memory_use,
-                 requested_processors, requested_time, requested_memory, status, user_id, group_id, executable,
-                 queue_number, partition_number, preceding_job_id, think_time, wait_time, ignore_memory=True):
+    def __init__(self, job_id, submission_time, execution_time,
+                 processors_allocated, average_cpu_use, memory_use,
+                 requested_processors, requested_time, requested_memory,
+                 status, user_id, group_id, executable, queue_number,
+                 partition_number, preceding_job_id, think_time, wait_time,
+                 ignore_memory=True):
         self.id = job_id
         self.submission_time = submission_time
         self.execution_time = execution_time
@@ -60,7 +88,8 @@ class Job(object):
         self.slot_position = None
 
     def __str__(self):
-        return f'Job<{self.id}, {self.status.name}, start={self.start_time}, processors={self.requested_processors}, ' \
+        return f'Job<{self.id}, {self.status.name}, start={self.start_time}, '\
+            f'processors={self.requested_processors}, ' \
             f'memory={self.requested_memory} ' \
             f'duration={self.execution_time}>'
 
@@ -68,15 +97,26 @@ class Job(object):
 
     @property
     def proper(self):
+        """Checks whether this job is a proper job with assigned resources.
+
+        Returns:
+            bool: True if the job is proper, and False otherwise.
+        """
         processors, memory = self.resources.measure()
-        return processors == self.requested_processors and (self.ignore_memory or memory == self.requested_memory)
+        return processors == self.requested_processors and \
+            (self.ignore_memory or memory == self.requested_memory)
 
     @property
     def slowdown(self):
+        """Computes the slowdown of the current job."""
         try:
-            return (self.finish_time - self.submission_time) / self.execution_time
+            return ((self.finish_time - self.submission_time) /
+                    self.execution_time)
         except TypeError:
-            warnings.warn(f"Failed to obtain slowdown for job {self}. It may not have finished yet.")
+            warnings.warn(
+                f"Failed to obtain slowdown for job {self}. "
+                "It may not have finished yet."
+            )
             return -1
 
     @property
@@ -112,11 +152,45 @@ class Job(object):
 
     @property
     def swfstatus(self):
+        """Returns the job status in the format expected by the SWF."""
         if self.status == JobStatus.COMPLETED:
             return SwfJobStatus.COMPLETED
         return SwfJobStatus.MEANINGLESS
 
-class JobParameters(object):
+
+class JobParameters:
+    """Class for using with generative models for job creation.
+
+    Assumes two types of jobs:
+        1. "Small" jobs and
+        2. "Large" jobs
+
+    A job has probability s of being small and (1-s) of being large.
+
+    Moreover, jobs have a dominant resource to distinguish between CPU-bound
+    and I/O bound jobs, with probability of being either CPU-bound and I/O
+    bound
+    0.5.
+
+    A user of this class must specify all bounds.
+
+    Parameters
+    ----------
+        lower_time_bound : int
+            The minimum time a job will run for
+        upper_time_bound : int
+            The maximum time a job will run for
+        lower_cpu_bound : int
+            The minimum number of processors a job will consume
+        upper_cpu_bound : int
+            The maximum number of processors a job will consume
+        lower_mem_bound : int
+            The minimum amount of memory a job will consume
+        upper_mem_bound : int
+            The maximum amount of memory a job will consume
+
+    Used by :class:`lugarrl.workload.distribution.BinomialWorkloadGenerator`.
+    """
     lower_time_bound: int
     upper_time_bound: int
     lower_resource_bound: int
@@ -126,11 +200,15 @@ class JobParameters(object):
     def _validate_parameters(*args):
         for param in args:
             if param <= 0:
-                raise AssertionError("Unable to work with non-positive bounds.")
+                raise AssertionError(
+                    "Unable to work with non-positive bounds."
+                )
 
-    def __init__(self, lower_time_bound: int, upper_time_bound: int, lower_cpu_bound: int,
-                 upper_cpu_bound: int, lower_mem_bound: int, upper_mem_bound: int):
-        self._validate_parameters(lower_time_bound, upper_time_bound, lower_cpu_bound, upper_cpu_bound,
+    def __init__(self, lower_time_bound: int, upper_time_bound: int,
+                 lower_cpu_bound: int, upper_cpu_bound: int,
+                 lower_mem_bound: int, upper_mem_bound: int):
+        self._validate_parameters(lower_time_bound, upper_time_bound,
+                                  lower_cpu_bound, upper_cpu_bound,
                                   lower_mem_bound, upper_mem_bound)
 
         self.lower_time_bound = lower_time_bound
@@ -141,27 +219,42 @@ class JobParameters(object):
         self.upper_mem_bound = upper_mem_bound
 
         self.resource_samplers = {
-            PrimaryResource.CPU: lambda: random.randint(self.lower_cpu_bound, self.upper_cpu_bound),
-            PrimaryResource.MEMORY: lambda: random.randint(self.lower_mem_bound, self.upper_mem_bound),
+            PrimaryResource.CPU: lambda: random.randint(self.lower_cpu_bound,
+                                                        self.upper_cpu_bound),
+            PrimaryResource.MEMORY:
+                lambda: random.randint(self.lower_mem_bound,
+                                       self.upper_mem_bound),
         }
 
         self.job_id = 1
         self.time_step = 0
 
     def add_time(self, steps: int = 1) -> None:
+        """Increments time in the internal counter.
+        """
         if steps < 0:
             raise AssertionError("Time can't be negative.")
         self.time_step += steps
 
     def sample(self, submission_time: int = 0) -> Job:
-        time_duration = random.randint(self.lower_time_bound, self.upper_time_bound)
+        """Samples a new job.
+
+        Parameters
+        ----------
+            submission_time : int
+                The time at which the new sampled job would have been
+                submitted. If omitted, the current times step is used.
+        """
+        time_duration = random.randint(self.lower_time_bound,
+                                       self.upper_time_bound)
 
         cpu = self.resource_samplers[PrimaryResource.CPU]()
         mem = self.resource_samplers[PrimaryResource.MEMORY]()
 
         job = Job(
-            self.job_id, submission_time if submission_time else self.time_step, time_duration, cpu, 0, mem, cpu,
-            time_duration, mem, JobStatus.WAITING, 1, 1, 1, 1, 1, -1, -1, -1
+            self.job_id, submission_time if submission_time else
+            self.time_step, time_duration, cpu, 0, mem, cpu, time_duration,
+            mem, JobStatus.WAITING, 1, 1, 1, 1, 1, -1, -1, -1
         )
         self.job_id += 1
 
