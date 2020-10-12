7d5d4de6c93c202ec812afae4d2045e959d783f9
Renato L. F. Cunha <renatocunha@acm.org>
2020-10-11 21:48:44 -0300

    docs: add package directory to documentation

--- docs/conf.py
+++ docs/conf.py
@@ -10,9 +10,11 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
+import lugarrl
 
 
 # -- Project information -----------------------------------------------------
@@ -28,7 +30,9 @@ author = 'Renato L. de F. Cunha'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'recommonmark'
+    'sphinx.ext.autodoc',
+    'sphinx.ext.coverage',
+    'sphinx.ext.napoleon',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
