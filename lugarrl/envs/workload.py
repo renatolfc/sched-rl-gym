dcef189cf2a533c0f9c272bdc29a2cf274c75eef
Renato L. F. Cunha <renatocunha@acm.org>
2021-02-06 16:52:18 -0300

    code(workload): add support for configurable (user) runtime estimates

--- lugarrl/envs/workload.py
+++ lugarrl/envs/workload.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import random
+import warnings
 import itertools
 from math import log2
 from typing import Optional
@@ -103,42 +104,88 @@ class DeepRmWorkloadGenerator(wl.DistributionalWorkloadGenerator):
 
 
 class SyntheticWorkloadGenerator(wl.TraceGenerator):
+    """A synthetic workload generator based on realistic models."""
     def __init__(self, length, nodes, start_time=8, random_seed=0,
-                 restart=False, runtime_estimates=True):
+                 restart=False, uniform_proportion=.7,
+                 runtime_estimates=None, estimate_parameters=None):
         """Synthetic workload generator based on Lublin's work.
 
         Parameters
         ----------
-            length : number of jobs to generate
-            nodes : number of compute nodes in the system
-            start_time : hour of day in which to start simulation
-            random_seed : random seed to use to generate jobs
-            restart : whether to restart after a sample finishes
+            length : int
+                number of jobs to generate
+            nodes : int
+                number of compute nodes in the system
+            start_time : int
+                hour of day in which to start simulation
+            random_seed : int
+                random seed to use to generate jobs
+            restart : bool
+                whether to restart after a sample finishes
+            runtime_estimates : {'gaussian', 'tsafrir', None}
+                whether to include runtime estimates and the method used
+                to compute them:
+                * None generates perfect estimate (estimates equal run time)
+                * 'gaussian' generates estimates with zero-mean Gaussian noise
+                  added to them
+                * 'tsafrir' uses Dan Tsafrir's model of user runtime estimates
+                  to generate estimates
+            estimate_parameters : Union[float, List[Tuple[float, float]]
+                the parameters used for generating user estimates.
+                Depends on :param:`runtime_estimates`.
+                When `runtime_estimates` is 'gaussian', this is a single
+                floating-point number that sets the standard deviation of the
+                noise.
+                When `runtime_estimates` is 'tsafrir', this is a list of
+                floating-point pairs specifying a histogram (time, number of
+                jobs) of job runtime popularity.
         """
-        self.lublin = Lublin99(True, random_seed)#, length)
+        self.lublin = Lublin99(True, random_seed, length)
         self.lublin.start = start_time
+        self.random_seed = random_seed
+        random.seed(random_seed)
 
-        uLow = .8
-        uProb = .7
-        uHi = log2(nodes)
-        uMed = ((uHi - 1.5) + (uHi - 3.5)) / 2
+        uniform_low_prob = .8
+        log2_size = log2(nodes)
+        breaking_point = ((log2_size - 1.5) + (log2_size - 3.5)) / 2
 
         self.lublin.setParallelJobProbabilities(
-            False, uLow, uMed, uHi, uProb
+            False, uniform_low_prob, breaking_point, log2_size,
+            uniform_proportion
         )
         self.lublin.setParallelJobProbabilities(
-            True, uLow, uMed, uHi, uProb
+            True, uniform_low_prob, breaking_point, log2_size,
+            uniform_proportion
         )
 
         self.runtime_estimates = runtime_estimates
+        self.estimate_parameters = estimate_parameters
+
         trace = self.refresh_jobs()
         super().__init__(restart, trace)
 
     def refresh_jobs(self):
+        "Refreshes the underlying job list."
         jobs = self.lublin.generate()
         if self.runtime_estimates:
-            tsafrir = Tsafrir05(jobs)
-            jobs = tsafrir.generate(jobs)
+            if self.runtime_estimates == 'tsafrir':
+                if self.estimate_parameters is not None:
+                    warnings.warn(
+                        'Setting tsafrir parameters is currently unsupported'
+                    )
+                tsafrir = Tsafrir05(jobs)
+                jobs = tsafrir.generate(jobs)
+            elif self.runtime_estimates == 'gaussian':
+                for j in jobs:
+                    j.reqTime = random.gauss(
+                        j.runTime,
+                        self.estimate_parameters * j.runTime
+                    )
+            else:
+                raise ValueError(
+                    f'Unsupported estimate type {self.runtime_estimates}'
+                )
+
         self.trace = [job.Job.from_swf_job(j) for j in jobs]
         return self.trace
 
