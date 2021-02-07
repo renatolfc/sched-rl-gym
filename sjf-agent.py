21950c580a2a1eb65fd76e922ce3306900333431
Renato L. F. Cunha <renatocunha@acm.org>
2021-02-07 00:18:25 -0300

    code(sjf): make agent robust to number of resources available

--- sjf-agent.py
+++ sjf-agent.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import os
 import gym
+import json
 import numpy as np
 import lugarrl.envs as deeprm
 
@@ -11,15 +13,17 @@ MAX_EPISODE_LENGTH = 200
 
 def sjf_action(observation):
     "Selects the job SJF (Shortest Job First) would select."
-    best = observation[2].shape[1] + 1  # infinity
-    best_idx = observation[2].shape[0]
-    free_proc = observation[0].shape[1] - np.sum(observation[0][0, :] != 0)
-    free_mem = observation[1].shape[1] - np.sum(observation[1][0, :] != 0)
-    for slot in range(observation[2].shape[0]):
-        used_proc = (observation[2][slot, 0, :] != 0).sum()
-        used_mem = (observation[3][slot, 0, :] != 0).sum()
-        if used_proc and used_proc <= free_proc and used_mem <= free_mem:
-            tmp = np.sum(observation[2][slot, :, 0])
+
+    current, wait, _, _ = observation
+    best = wait.shape[2] + 1  # infinity
+    best_idx = wait.shape[1]
+
+    free = np.ones(current.shape[0]) * current.shape[-1] - np.sum(current[:, 0, :] != 0)
+
+    for slot in range(wait.shape[1]):
+        required_resources = (wait[:, slot, 0, :] != 0).sum(axis=1)
+        if np.all(required_resources <= free):
+            tmp = np.sum(wait[0, slot, :, 0])
             if tmp < best:
                 best_idx = slot
                 best = tmp
@@ -34,20 +38,22 @@ def find_position(q, idx):
 
 
 def pack_observation(ob, time_horizon):
-    ob = list(ob)
-    ob[2] = np.hstack(ob[2]).reshape((time_horizon, -1))
-    ob[3] = np.hstack(ob[3]).reshape((time_horizon, -1))
-    return np.hstack(ob)
-
+    current, wait, backlog, time = ob
+    wait = wait.reshape(time_horizon, -1)
+    current = current.reshape(time_horizon, -1)
+    return np.hstack((current, wait, backlog, time))
 
 def main():
-    env: deeprm.DeepRmEnv = gym.make('DeepRM-v0', use_raw_state=True)
-    env.use_raw_state = True
-    time_horizon = env.observation_space[0].shape[0]
+    kwargs = {'use_raw_state': True}
+    if os.path.exists('config/test.json'):
+        with open('config/test.json', 'r') as fp:
+            kwargs = json.load(fp)
+    env: deeprm.DeepRmEnv = gym.make('DeepRM-v0', **kwargs)
+    time_horizon = env.reset()[0].shape[1]
     for episode in range(EPISODES):
         ob = env.reset()
         action = sjf_action(ob)
-        for _ in range(200):
+        while True:
             ob, reward, done, _ = env.step(action)
             action = sjf_action(ob)
             ob = pack_observation(ob, time_horizon)
