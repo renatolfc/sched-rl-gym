2044b5926641ac57d54afacc2f442ee276787afe
Renato L. de F. Cunha <renatocunha@acm.org>
2021-12-30 16:55:27 +0000

    code(envs): make environments accessible from package

--- schedgym/envs/__init__.py
+++ schedgym/envs/__init__.py
@@ -4,16 +4,19 @@
 import logging
 from gym.envs.registration import register
 
+from .deeprm_env import DeepRmEnv
+from .compact_env import CompactRmEnv
+
 logger = logging.getLogger(__name__)
 
 register(
     id='DeepRM-v0',
     nondeterministic=False,
-    entry_point='schedgym.envs.deeprm_env:DeepRmEnv',
+    entry_point=f'schedgym.envs.deeprm_env:{DeepRmEnv.__name__}',
 )
 
 register(
     id='CompactRM-v0',
     nondeterministic=False,
-    entry_point='schedgym.envs.compact_env:CompactRmEnv',
+    entry_point=f'schedgym.envs.compact_env:{CompactRmEnv.__name__}',
 )
